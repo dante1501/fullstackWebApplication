c9046281e930f91fa0821aa1e36aff34f9a6e76a
dante1501 <83389073+dante1501@users.noreply.github.com>
2024-06-29 13:38:00 +0530

    Initial project creation
    Added Project Structure

---
+++ src/test/java/com/sanky/fullstackapplication/WebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanky.fullstackapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
